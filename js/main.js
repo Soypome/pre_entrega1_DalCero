0428341d81f49ef82489e348a30de512cae0ba82
Nahuel Dal Cero <nahueldalcero@gmail.com>
2023-01-06 16:17:47 -0300

    pre_entrega1_Dal_Cero

---
+++ js/main.js
@@ -0,0 +1,65 @@
+alert("Bienvenido al cotizador de juegos online");
+let carritoFinal = 0;
+let pregunta = "";
+const precioJuego = parseInt(
+  prompt("Digite el precio en pesos argentinos del juego que desea comprar: ")
+);
+
+function calculoImpuestos(gcias, bsPers, PAIS) {
+  return gcias + bsPers + PAIS;
+}
+const impuestos = calculoImpuestos(0.3, 0.15, 0.3);
+
+carritoFinal = precioJuego + impuestos * precioJuego;
+console.log("El valor de su juego es de: $" + carritoFinal);
+
+pregunta = prompt("¿Desea añadir otro juego?");
+
+while (pregunta === "si" || pregunta === "Si" || pregunta === "SI") {
+  let precioNuevo = parseInt(prompt("Digite el precio del nuevo juego: "));
+
+  let precioConImpuestos = precioNuevo * impuestos;
+
+  let precioNuevoConImpuestos = precioConImpuestos + precioNuevo;
+
+  carritoFinal = carritoFinal + precioNuevoConImpuestos;
+
+  console.log("El valor de su nuevo juego es: $" + precioNuevoConImpuestos);
+
+  console.log("El total a pagar es de: $" + carritoFinal);
+
+  pregunta = prompt("¿Desea añadir otro juego?");
+}
+
+alert("¡Muchas Gracias!");
+
+/* do {
+  let precioNuevo = parseInt(prompt("Digite el precio del nuevo juego: "));
+  
+  let precioConImpuestos = precioNuevo * impuestos;
+  
+  let precioNuevoConImpuestos = precioConImpuestos + precioNuevo;
+  
+  carritoFinal = carritoFinal + precioNuevoConImpuestos;
+  
+  console.log("El valor de su nuevo juego es: $" + precioNuevoConImpuestos);
+  
+  console.log("El total a pagar es de: $" + carritoFinal);
+
+  let pregunta = prompt("¿Desea añadir otro juego?"); */
+/* if (pregunta === "si" || pregunta === "Si" || pregunta === "SI") {
+  let precioNuevo = parseInt(prompt("Digite el precio del nuevo juego: "));
+
+  let precioConImpuestos = precioNuevo * impuestos;
+  console.log(precioConImpuestos);
+
+  let precioNuevoConImpuestos = precioConImpuestos + precioNuevo;
+
+  precioFinal = primerPrecioFinal + precioNuevoConImpuestos;
+
+  console.log("El valor de su nuevo juego es: $" + precioNuevoConImpuestos);
+
+  console.log("El total a pagar es de: $" + precioFinal);
+} else {
+  alert("¡Muchas gracias!");
+} */
